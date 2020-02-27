e42c3e0305e5d519f589604d91d0d1537090d45c
zyk <602836219@qq.com>
2020-02-27 21:07:07 +0800

    Initial commit

---
+++ app/src/main/java/com/zyknet/ble_hrv/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zyknet.ble_hrv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
